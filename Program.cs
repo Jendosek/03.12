f5019a222d9e94056973c1c96c86bd0c93a81a87
Jendosek <bala01081977@gmail.com>
2024-12-07 23:48:35 +0200

    hm3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using _03._12.Entities;
 
 namespace _03._12;
 
@@ -9,5 +10,9 @@ class Program
         Console.InputEncoding = Encoding.Unicode;  
         Console.OutputEncoding = Encoding.Unicode;
         
+        var arr = new Arr(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 11 });
+        Console.WriteLine(arr.Print());
+        Console.WriteLine(arr.CountDistinct());
+        Console.WriteLine(arr.EqualToValue(5));
     }
 }
